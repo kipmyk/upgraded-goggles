0c21e5e66d6a324f5b6690c0a08214c0d22e9c88
kipmyk <trajanhacker@gmail.com>
2020-12-25 12:09:16 +0300

    manual links

--- functions.php
+++ functions.php
@@ -10,9 +10,6 @@ function university_files(){
 add_action('wp_enqueue_scripts','university_files');
 
 function university_features(){
-    register_nav_menu('headerMenuLocation','Header Menu Location');
-    register_nav_menu('footerMenuLocationOne','Footer Menu Location One');
-    register_nav_menu('footerMenuLocationTwo','Footer Menu Location Two');
     add_theme_support('title-tag');
 }
 add_action('after_setup_theme', 'university_features');
